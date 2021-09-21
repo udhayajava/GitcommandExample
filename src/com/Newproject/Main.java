a66a870df167ee09bfa135449b4635d1ace04ef8
Udhayakumar <udhayaarjvit1060>
2021-09-21 11:06:07 +0530

    sample commit

--- src/com/Newproject/Main.java
+++ src/com/Newproject/Main.java
@@ -3,6 +3,9 @@ package com.Newproject;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// this is the program is make me to understand the uses of git
+        int a=10; int b=20; int c;
+        System.out.println(c=a+b);
+
     }
 }
